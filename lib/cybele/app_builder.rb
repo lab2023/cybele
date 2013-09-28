fe6b690f4d2b88be0cd8569e6c62cab0debaa829
Onur Ozgur OZKAN <onur.ozgur.ozkan@lab2023.com>
2013-09-28 22:02:23 +0300

    #87 add time_zone to devise model

--- lib/cybele/app_builder.rb
+++ lib/cybele/app_builder.rb
@@ -264,7 +264,7 @@ require 'capybara/rspec'
 class #{model_name.classify}::ParameterSanitizer < Devise::ParameterSanitizer
   private
   def sign_up
-    default_params.permit(:name, :email, :password, :password_confirmation) # TODO add other params here
+    default_params.permit(:name, :email, :password, :password_confirmation, :time_zone) # TODO add other params here
   end
 end
       CODE
