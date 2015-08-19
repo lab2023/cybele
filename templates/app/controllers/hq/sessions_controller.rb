952a035386ba0f896cc64453840d592c7eb4e57d
Ismail Akbudak <ismail.akbudak@lab2023.com>
2015-08-19 11:06:56 +0300

    add devise return path

--- templates/app/controllers/hq/sessions_controller.rb
+++ templates/app/controllers/hq/sessions_controller.rb
@@ -1,3 +1,16 @@
 class Hq::SessionsController < Devise::SessionsController
   layout 'login'
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_in_path_for(resource_or_scope)
+    hq_root_path
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path
+  end
+  
 end
