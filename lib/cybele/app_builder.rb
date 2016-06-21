e2dc3905a70b370cb6194ce54ed81ae2baf34841
ismail Akbudak <ismail.akbudak@lab2023.com>
2016-06-21 11:16:24 +0300

    add new config to bullet

--- lib/cybele/app_builder.rb
+++ lib/cybele/app_builder.rb
@@ -141,6 +141,9 @@ config.after_initialize do
     Bullet.enable = true
     Bullet.alert = true
     Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = false
   end
       RUBY
       configure_environment 'development', config
