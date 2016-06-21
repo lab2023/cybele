0c104e36f15b62b5034cf445f6cd83ec63dcaf96
ismail Akbudak <ismail.akbudak@lab2023.com>
2016-06-21 18:09:18 +0300

    fix small details

--- templates/app/controllers/user/user_application_controller.rb
+++ templates/app/controllers/user/user_application_controller.rb
@@ -2,6 +2,7 @@ require 'application_responder'
 
 class User::UserApplicationController < ActionController::Base
   # layout 'user/application'
+  layout 'application'
   before_action :authenticate_user!
   self.responder = ApplicationResponder
   respond_to :html, :json
