8e420a5b94dd9b55d6722a4852b60a304e97832f
İsmail Akbudak <ismail.akbudak@lab2023.com>
2016-08-03 14:29:31 +0300

    #122 add before filter for time zone setter method

--- templates/app/controllers/concerns/basic_authentication.rb
+++ templates/app/controllers/concerns/basic_authentication.rb
@@ -8,7 +8,7 @@ module BasicAuthentication
   private
 
   def authenticate
-    if Rails.env.staging? and ENV['BASIC_AUTH_IS_ACTIVE'] == 'yes'
+    if ENV['BASIC_AUTH_IS_ACTIVE'] == 'yes'
       authenticate_or_request_with_http_basic do |username, password|
         username == Settings.basic_auth.username && password == Settings.basic_auth.password
       end
