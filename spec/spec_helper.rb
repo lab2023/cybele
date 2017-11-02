0b4b361fb5f8d6a513929b1f73dfc194a41b9a28
FatihAvsan <fatih.avsan@lab2023.com>
2017-11-02 10:24:52 +0300

    KBP-137 #time 15m - added spec_helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,20 @@ require source_path
 
 Dir['./spec/support/**/*.rb'].each { |file| require file }
 
+def mail_test_helper(path)
+  file = content(path)
+  expect(file).to match('smtp')
+  expect(file).to match('address:')
+  expect(file).to match('port:')
+  expect(file).to match('enable_starttls_auto:')
+  expect(file).to match('user_name:')
+  expect(file).to match('password:')
+  expect(file).to match('authentication:')
+  unless content('config/settings.yml').present?
+    expect(file).to match('host:')
+  end
+end
+
 RSpec.configure do |config|
   config.include CybeleTestHelpers
 
