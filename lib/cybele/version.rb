b5b69905a7773fc9577464d97b0c1e3db9faa3a8
İsmail Akbudak <ismail.akbudak@lab2023.com>
2018-12-25 17:19:37 +0300

    Update version v2.2.0

--- lib/cybele/version.rb
+++ lib/cybele/version.rb
@@ -3,5 +3,5 @@
 module Cybele
   RAILS_VERSION = ['~> 5.2.2', '>= 5.2.2'].freeze
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
-  VERSION = '2.1.0'
+  VERSION = '2.2.0'
 end
